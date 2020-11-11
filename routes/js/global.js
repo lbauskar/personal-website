aff6c185b736ecaad0ff3e1ee44daad55fc5108d
Lalit <labauskar@gmail.com>
2020-11-10 20:13:32 -0600

    faster animations, changed resume

--- routes/js/global.js
+++ routes/js/global.js
@@ -1,19 +1,19 @@
 let GlobalHTML = { //adds HTML that should go on every webpage 
-    navbar: document.getElementById("navbar"),
-
-    navbarHTML:
-    `<div class="navbar">
-        <a id="nav-placeholder"> ;; </a>
-        <a href="/" id="nav-home"> Lalit Bauskar </a>
-        <a href="/about" id="nav-about"> About Me </a>
-        <a href="/portfolio" id="nav-portfolio"> Portfolio </a>
-        <a href="/qualifications" id="nav-qualifications"> Qualifications </a>
-        <a href="/service" id="nav-service"> Service </a>
-    </div>`,
-
     addNavbar: function() {
-        if (this.navbar) {
-            this.navbar.innerHTML = this.navbarHTML;
+        const navbar = document.getElementById("navbar");
+
+        const navbarHTML = 
+        `<div class="navbar">
+            <a id="nav-placeholder"> ;; </a>
+            <a href="/" id="nav-home"> Home </a>
+            <a href="/about" id="nav-about"> About Me </a>
+            <a href="/portfolio" id="nav-portfolio"> Portfolio </a>
+            <a href="/qualifications" id="nav-qualifications"> Qualifications </a>
+            <a href="/service" id="nav-service"> Service </a>
+        </div>`;
+
+        if (navbar) {
+            navbar.innerHTML = navbarHTML;
         }
     }
 }
--- start.js
+++ start.js
@@ -3,4 +3,5 @@ const app = require("./app");
 
 const server = app.listen(process.env.PORT || 5000, () => {
     console.log(`My Website (Express) is running on port ${server.address().port}`);
+    console.log(`local url: http://localhost:${server.address().port}`);
 });
