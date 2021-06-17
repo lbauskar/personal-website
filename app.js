4b466f6e415cea90a6965074c8d93dd1d858d7ef
Lalit <labauskar@gmail.com>
2021-06-17 16:33:29 -0500

    gzip files and redirect old pathnames to home page

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const routes = require("./lib/index");
 const path = require("path");
 const bodyParser = require("body-parser");
+const compression = require('compression');
 
 const app = express();
+app.use(compression());
 
 app.set("views", path.join(__dirname, "/views"));
 app.engine("html", require("ejs").renderFile);
