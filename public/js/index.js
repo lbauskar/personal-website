33f56ee20c7f3b46529dc57375d59fd783e3cb1a
lalit <labauskar@gmail.com>
2021-07-16 07:30:56 -0500

    appearance overhaul

--- public/js/index.js
+++ public/js/index.js
@@ -18,11 +18,10 @@ VANTA.NET({
 function typeWriter(element, text, i = 0) {
     if (i < text.length) {
         element.innerHTML += text.charAt(i);
-        setTimeout(typeWriter, 150, element, text, i+1);
+        setTimeout(typeWriter, 75, element, text, i+1);
     }
 }
-
-typeWriter(document.getElementById('name-span'), 'Lalit Bauskar');
+setTimeout(() => typeWriter(document.getElementById('name-span'), 'Lalit Bauskar'), 250);
 
 
 //scroll to element
@@ -44,11 +43,11 @@ for (let element of document.getElementsByClassName('stars')) {
 
     let file;
     if (element.classList.contains('5-star')) {
-        file = 'src/5stars.json';
+        file = 'src/lottie/5stars.json';
     } else if (element.classList.contains('4-star')) {
-        file = 'src/4stars.json';
+        file = 'src/lottie/4stars.json';
     } else if (element.classList.contains('3-star')) {
-        file = 'src/3stars.json';
+        file = 'src/lottie/3stars.json';
     }
 
     const animation = lottie.loadAnimation({
@@ -62,9 +61,11 @@ for (let element of document.getElementsByClassName('stars')) {
     stars.push(animation);
 }
 
+
+//contact button animations
 const linkedin = lottie.loadAnimation({
-    container: $('#linkedin-button')[0],
-    path: 'src/linkedin.json',
+    container: document.getElementById('linkedin-button'),
+    path: 'src/lottie/linkedin.json',
     renderer: 'svg',
     loop: true,
     autoplay: false
@@ -72,24 +73,87 @@ const linkedin = lottie.loadAnimation({
 linkedin.setSpeed(0.7);
 
 const github = lottie.loadAnimation({
-    container: $('#github-button')[0],
-    path: 'src/github.json',
+    container: document.getElementById('github-button'),
+    path: 'src/lottie/github.json',
     renderer: 'svg',
     loop: true,
     autoplay: false
 });
 github.setSpeed(3);
 
-
 const email = lottie.loadAnimation({
-    container: $('#email-button')[0],
-    path: 'src/email.json',
+    container: document.getElementById('email-button'),
+    path: 'src/lottie/email.json',
     renderer: 'svg',
     loop: true,
     autoplay: false
 });
 email.setSpeed(1.5);
 
+
+//contact buttons play on hover
+function addContactButtonListeners(contactButton, animation) {
+
+    contactButton.addEventListener('mouseover', () => {
+        animation.play();
+        contactButton.classList.remove('blue-filter');
+        contactButton.classList.add('white-filter');
+    });
+
+    contactButton.addEventListener('mouseout', () => {
+        animation.stop();
+        contactButton.classList.remove('white-filter');
+        contactButton.classList.add('blue-filter');
+    });
+}
+addContactButtonListeners(document.getElementById('linkedin-button'), linkedin);
+addContactButtonListeners(document.getElementById('github-button'), github);
+addContactButtonListeners(document.getElementById('email-button'), email);
+
+
+//elements of tiles slide in on hover
+const tiles = Array.from(document.getElementsByClassName('gallery-item'));
+for (const tile of tiles) {
+    const above = Array.from(tile.getElementsByClassName('some-above'));
+    const below = Array.from(tile.getElementsByClassName('some-below'));
+    tile.addEventListener('mouseover', () => {
+        for (const elem of above) {
+            elem.classList.add('final-pos');
+        }
+
+        for (const elem of below) {
+            elem.classList.add('final-pos');
+        }
+    });
+
+    tile.addEventListener('mouseout', () => {
+        for (const elem of above) {
+            elem.classList.remove('final-pos');
+        }
+
+        for (const elem of below) {
+            elem.classList.remove('final-pos');
+        }
+    });
+}
+
+//detail buttons show up when tabbed    
+const detailButtons = Array.from(document.querySelectorAll('.gallery-item button'));
+for (const button of detailButtons) {
+    const textDiv = button.parentNode;
+    button.addEventListener('focus', () => {
+        textDiv.style.opacity = '1';
+        for (const child of textDiv.children) {
+            child.classList.add('final-pos');
+        }
+    });
+    button.addEventListener('blur', () => {
+        textDiv.style.opacity = '';
+        for (const child of textDiv.children) {
+            child.classList.remove('final-pos');
+        }
+    });
+}
 
 //jquery stuff
 $(() => {
@@ -129,116 +193,19 @@ $(() => {
         $('.stars svg').attr('height', '40');
     });
 
-    //have stuff on about section slide in from left of screen
-    $('#about').appear();
-    $('#about').on('appear', () => {
-        const msDelay = 150;
-        const leftElems = $('#about .far-left').toArray();
-        for (let i = 0; i < leftElems.length; ++i) {
-            setTimeout(() => {leftElems[i].classList.add('final-pos')}, msDelay * i);
-        }
-    });
 
-    //have project tiles slide from below
-    $('#projects .gallery').appear();
-    $('#projects .gallery').on('appear', () => {
-        const msDelay = 200;
-        const elems = $('#projects .far-below').toArray();
-        for (let i = 0; i < elems.length; ++i) {
-            setTimeout(() => {elems[i].classList.add('final-pos')}, msDelay * i);
-        }
-    });
-
-    //have experience tiles gro
-    $('#experience .gallery').appear();
-    $('#experience .gallery').on('appear', () => {
-        const msDelay = 150;
-        const elems = $('#experience .tiny').toArray();
-        for (let i = 0; i < elems.length; ++i) {
-            setTimeout(() => {elems[i].classList.add('final-pos')}, msDelay * i);
-        }
-    })
-
-    //contact buttons play on hover
-    const linkedinButton = $('#linkedin-button')[0];
-    linkedinButton.addEventListener('mouseover', () => {
-        linkedin.play();
-        linkedinButton.classList.remove('blue-filter');
-        linkedinButton.classList.add('white-filter');
-    });
-    linkedinButton.addEventListener('mouseout', () => {
-        linkedin.stop();
-        linkedinButton.classList.remove('white-filter');
-        linkedinButton.classList.add('blue-filter');
-    });
-
-    const githubButton = $('#github-button')[0];
-    githubButton.addEventListener('mouseover', () => {
-        github.play();
-        githubButton.classList.remove('blue-filter');
-        githubButton.classList.add('white-filter');
-    });
-    githubButton.addEventListener('mouseout', () => {
-        github.stop();
-        githubButton.classList.remove('white-filter');
-        githubButton.classList.add('blue-filter');
-    });
-
-    const emailButton = $('#email-button')[0];
-    emailButton.addEventListener('mouseover', () => {
-        email.play();
-        emailButton.classList.remove('blue-filter');
-        emailButton.classList.add('white-filter');
-    });
-    emailButton.addEventListener('mouseout', () => {
-        email.stop();
-        emailButton.classList.remove('white-filter');
-        emailButton.classList.add('blue-filter');
-    });
-
-    //elements of tiles slide in on hover
-    const tiles = $('.gallery-item').toArray();
-    for (const tile of tiles) {
-        const above = tile.getElementsByClassName('some-above');
-        const below = tile.getElementsByClassName('some-below');
-        tile.addEventListener('mouseover', () => {
-            for (const elem of above) {
-                elem.classList.add('final-pos');
+    function animateOnAppearance(container, selector, msDelay) {
+        $(container).appear();
+        $(container).on('appear', () => {
+            const elems = $(`${container} ${selector}`).toArray();
+            for (let i = 0; i < elems.length; ++i) {
+                setTimeout(() => {elems[i].classList.add('final-pos')}, msDelay * i);
             }
-
-            for (const elem of below) {
-                elem.classList.add('final-pos');
-            }
-        });
-
-        tile.addEventListener('mouseout', () => {
-            for (const elem of above) {
-                elem.classList.remove('final-pos');
-            }
-
-            for (const elem of below) {
-                elem.classList.remove('final-pos');
-            }
-        });
+        })
     }
-
-    //detail buttons show up when tabbed    
-    const detailButtons = $('.gallery-item button').toArray();
-    for (const detailButton of detailButtons) {
-        const textDiv = detailButton.parentNode;
-        detailButton.addEventListener('focus', () => {
-            textDiv.style.opacity = '1';
-            for (const child of textDiv.children) {
-                child.classList.add('final-pos');
-            }
-        });
-        detailButton.addEventListener('blur', () => {
-            textDiv.style.opacity = '';
-            for (const child of textDiv.children) {
-                child.classList.remove('final-pos');
-            }
-        });
-    }
+    animateOnAppearance('#about', '.far-left', 150); //slide from left in about section
+    animateOnAppearance('#projects', '.far-below', 200); //slide from below in projects section
+    animateOnAppearance('#experience', '.tiny', 150); //grow in experience section
 });
 
 
